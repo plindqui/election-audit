cd094a644e5eb7167b444603bda05544479728d8
plindqui <paul.cindy.lindquist@gmail.com>
2016-05-30 14:38:18 -0500

    Add files via upload

---
+++ ElectionAudit/ImageParse/FileEntry.vb
@@ -0,0 +1,21 @@
+﻿
+Public Class FileEntry
+
+    Public Enum FileTypeEnum
+        File
+        CompressedFile
+    End Enum
+
+    Public Property FilePath As String
+    Public Property ArchivePath As String
+    Public Property FileType As FileTypeEnum
+    Public Property ParentPath As String
+
+    Public Sub New(ByVal filePath As String, ByVal archivePath As String, ByVal fileType As FileTypeEnum, ByVal parentPath As String)
+        Me.FilePath = filePath
+        Me.ArchivePath = archivePath
+        Me.FileType = fileType
+        Me.ParentPath = parentPath
+    End Sub
+
+End Class   'FileEntry class
---
+++ ElectionAudit/ImageParse/FileHandler.vb
@@ -0,0 +1,68 @@
+﻿Imports System.IO
+
+Public Class FileHandler
+    Private _fileList As New List(Of FileEntry)
+    Private _parentPath As String
+
+    Public Sub ForEachFileAndFolder(ByVal sourceFolder As String, _
+                                    ByVal directoryCallBack As Action(Of DirectoryInfo), _
+                                    ByVal fileCallBack As Action(Of FileInfo))
+
+        If Directory.Exists(sourceFolder) Then
+            Try
+                For Each foldername As String In Directory.GetDirectories(sourceFolder)
+                    If directoryCallBack IsNot Nothing Then
+                        directoryCallBack.Invoke(New DirectoryInfo(foldername))
+                    End If
+
+                    ForEachFileAndFolder(foldername, directoryCallBack, fileCallBack)
+                Next
+            Catch ex As UnauthorizedAccessException
+                Trace.TraceWarning(ex.Message)
+            End Try
+
+            If fileCallBack IsNot Nothing Then
+                Dim fileList As List(Of String) = Directory.GetFiles(sourceFolder).ToList
+                Dim sortedFileList As List(Of String) = fileList.OrderBy(Function(x) New FileInfo(x).Name.Substring(2)).ToList
+                For Each filename As String In sortedFileList
+                    fileCallBack.Invoke(New FileInfo(filename))
+                Next
+            End If
+        End If
+    End Sub
+
+    Public ReadOnly Property Count() As Integer
+        Get
+            Return _fileList.Count
+        End Get
+    End Property
+
+    Public ReadOnly Property Files() As List(Of FileEntry)
+        Get
+            Return _fileList
+        End Get
+    End Property
+
+    Public Sub DirectoryAction(ByVal dirInfo As DirectoryInfo)
+        _parentPath = dirInfo.FullName
+    End Sub
+
+    Public Sub FileAction(ByVal fileInfo As FileInfo)
+        If fileInfo.Extension = ".zip" Then
+            Try
+                Using archive As ZipArchive = ZipFile.OpenRead(fileInfo.FullName)
+                    For Each entry As ZipArchiveEntry In archive.Entries
+                        If entry.FullName.EndsWith(".pbm", StringComparison.OrdinalIgnoreCase) Then
+                            _fileList.Add(New FileEntry(entry.FullName, fileInfo.FullName, FileEntry.FileTypeEnum.CompressedFile, _parentPath))
+                        End If
+                    Next
+                End Using
+            Catch e As Exception
+                'Consume the exception
+            End Try
+        ElseIf fileInfo.Extension = ".pbm" Then
+            _fileList.Add(New FileEntry(fileInfo.Name, "", FileEntry.FileTypeEnum.File, _parentPath))
+        End If
+    End Sub
+
+End Class   'FileHandler class
