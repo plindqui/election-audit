ba01eeda3cfab8d39e6b1911d991d3a8fd378354
plindqui <paul.cindy.lindquist@gmail.com>
2016-05-30 14:31:58 -0500

    Create SampleForm.vb

---
+++ ElectionAudit/Form/SampleForm.vb
@@ -0,0 +1,7 @@
+Public Class SampleForm
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtSampleBallots.Text = String.Empty
+    End Sub
+
+End Class   'SampleForm class
